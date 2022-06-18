9b98c197392e14d2b9416bc7fe358153060573f8
Ekaterina <mishelledale@gmail.com>
2022-06-18 13:23:08 +0300

    Task 1: Find the ruslt of A^B is done

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Возведем число А в степень равную натуральному числу В");
+Console.WriteLine("Введите число А");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число В");
+int B = Convert.ToInt32(Console.ReadLine());
+
+if (B < 0)
+{
+    int B_pos = B*(-1);
+    double sum = Math.Pow(A, B_pos);
+    Console.WriteLine(sum);
+}
+if (B > 0)
+{
+    double sum = Math.Pow(A, B);
+    Console.WriteLine(sum);
+}
