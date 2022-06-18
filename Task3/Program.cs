16db310f5817eaee46fbeb0318a3103ca02c9968
Ekaterina <mishelledale@gmail.com>
2022-06-18 13:55:25 +0300

    Task3: Print the random 8digital massive is done

---
+++ Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] array = new int[8];
+FillArray(array);
+
+void FillArray(int[] array)
+{
+    int length = array.Length;
+    for (int i = 0; i < length; i++)
+    {
+       array[i] = new Random().Next(0, 100); 
+    }
+}
+void PrintArray(int[] array)
+{
+    int length = array.Length;
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write ($"{array[i]} ");
+    }
+}
+
+PrintArray(array);
