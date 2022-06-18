f6684b0ba432cd9e92f81f8b94ff228beeec21e3
Ekaterina <mishelledale@gmail.com>
2022-06-18 14:11:05 +0300

    Task2: Find the sum of all number digits is done

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Найдем сумму цифр введенного числа");
+Console.WriteLine("Введите число: ");
+int num, sum = 0, r;
+num = Convert.ToInt32(Console.ReadLine());
+while (num != 0)
+    {
+    r = num %10;
+    num = num / 10;
+    sum = sum + r;
+    }
+Console.WriteLine("Сумма цифр числа: " + sum);
+Console.ReadLine();
+ 
+ 
+ 
+        
